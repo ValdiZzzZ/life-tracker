3009f5028eabb86d495bc9cbcf5d1b9d9307a9b3
ValdiZzzZ <163528475+ValdiZzzZ@users.noreply.github.com>
2026-05-31 11:44:32 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE_NAME = "life-tracker-pwa-v2";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icons/icon-192.png", "./icons/icon-512.png"];
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+self.addEventListener("fetch", event => {
+  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
+});
